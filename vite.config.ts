2bf87cc9df4c0c693abf1d993a9a0af7940c204b
spwoodcock <sam.woodcock@protonmail.com>
2024-04-25 18:19:30 +0100

    build: add base to vite conifg for url path prefix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/xlsform-editor/",
   server: {
     port: 3000,
     host: '0.0.0.0',
