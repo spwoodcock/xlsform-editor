aae71fd671ff3e5465e977cf861312b86d3d2979
spwoodcock <sam.woodcock@protonmail.com>
2024-04-25 23:22:19 +0100

    fix: remove base path prefix after custom domain issued

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [],
-  base: "/xlsform-editor/",
+  // base: "/xlsform-editor/",
   server: {
     port: 3000,
     host: '0.0.0.0',
